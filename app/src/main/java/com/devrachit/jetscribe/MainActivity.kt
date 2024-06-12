828f0aac2f9c08affe4e9b4f39a0db8223464c78
rockyhappy <115190222+rockyhappy@users.noreply.github.com>
2024-06-12 20:39:36 +0530

    Status and Navigation Theme Changed

--- app/src/main/java/com/devrachit/jetscribe/MainActivity.kt
+++ app/src/main/java/com/devrachit/jetscribe/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.devrachit.jetscribe
 
+import android.app.Activity
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -13,8 +14,13 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.view.WindowInsetsControllerCompat
 import com.devrachit.jetscribe.presentation.navigation.App
+import com.devrachit.jetscribe.ui.theme.GrayShade2
+import com.devrachit.jetscribe.ui.theme.GrayShade4
 import com.devrachit.jetscribe.ui.theme.JetscribeTheme
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -24,7 +30,15 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+        window.statusBarColor= GrayShade2.toArgb()
+        window.navigationBarColor = GrayShade2.toArgb()
+
         setContent {
+            val activity = LocalContext.current as? Activity
+            activity?.window?.let { window ->
+                WindowInsetsControllerCompat(window, window.decorView).isAppearanceLightStatusBars =
+                    false
+            }
             JetscribeTheme {
                 Surface(
                     modifier = Modifier.fillMaxSize(),
